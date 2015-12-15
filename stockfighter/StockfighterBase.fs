5a8de87ae3f859c6ce1b6c212d6c0300e0d78544
bartosz.alksnin <bartek@gopromo.pl>
2015-12-15 20:51:30 +0000

    refactoring, fixing bugs

---
+++ stockfighter/StockfighterBase.fs
@@ -0,0 +1,78 @@
+module StockfigherCommon
+  open System
+  open System.Text
+  open System.Web.Script.Serialization
+  open HttpClient
+  open FSharp.Data
+  open FSharp.Data.JsonExtensions
+
+  type Direction =
+    | Buy
+    | Sell
+
+  type OrderType =
+    | Limit
+    | Market
+    | FillOrKill
+    | ImmidiateOrCancel
+
+  type Fills = {
+      price: int;
+      qty: int;
+      ts: String
+  }
+
+  type MakeOrderResponse(response : String) =
+    let info = JsonValue.Parse(response)
+    member this.ok: bool = info?ok.AsBoolean()
+    member this.symbol: String = info?symbol.AsString()
+    member this.venue: String = info?venue.AsString()
+    member this.direction: String = info?direction.AsString()
+    member this.originalQty: int = info?originalQty.AsInteger()
+    member this.qty: int = info?qty.AsInteger()
+    member this.price: int = info?price.AsInteger()
+    member this.orderType: String = info?orderType.AsString()
+    member this.id: int = info?id.AsInteger()
+    member this.account: String = info?account.AsString()
+    member this.ts: String = info?ts.AsString()
+    //member this.fills: List<Fills> = info?fills
+    member this.totalFilled: int = info?totalFilled.AsInteger()
+    member this.isOpen: bool = info?opens.AsBoolean()
+
+
+  type Order (account: String, venue: String, symbol: String, price : int, qty: int, direction: Direction, orderType: OrderType) =
+      member this.account = account
+      member this.venue = venue
+      member this.symbol = symbol
+      member this.price = price
+      member this.qty = qty
+      member this.direction =
+        match direction with
+          | Buy -> "Buy"
+          | Sell -> "Sell"
+      member this.orderType =
+        match orderType with
+          | Limit -> "limit"
+          | Market -> "market"
+          | FillOrKill -> "fill-or-kill"
+          | ImmidiateOrCancel -> "immediate-or-cancel"
+
+  let baseUrl = "https://api.stockfighter.io/ob/api"
+  let deserializer = (System.Web.Script.Serialization.JavaScriptSerializer())
+
+  let serialise obj =
+    (System.Web.Script.Serialization.JavaScriptSerializer()).Serialize(obj)
+
+  let post url bodyObject apiKey =
+      let body = serialise bodyObject
+      createRequest Post url
+      |> withHeader (Custom {name="X-Starfighter-Authorization"; value=apiKey})
+      |> withBody body
+
+  let get url apiKey =
+      createRequest Get url
+      |> withHeader (Custom {name="X-Starfighter-Authorization"; value=apiKey})
+
+  let delete url apiKey =
+    createRequest Delete url
+    |> withHeader (Custom {name="X-Starfighter-Authorization"; value=apiKey})
---
+++ stockfighter/stockfighterFunctional.fs
@@ -0,0 +1,6 @@
+module StockfighterFunctional
+  open System
+  open System.Text
+  open System.Web.Script.Serialization
+  open HttpClient
+  open FSharp.Data
