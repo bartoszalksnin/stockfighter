37da117cd43b8730107efe6c63703a3b48f19d49
bartosz.alksnin <bartek@gopromo.pl>
2016-01-03 00:22:19 +0000

    use paket boostraper

--- build.sh
+++ build.sh
@@ -1,2 +1,15 @@
-mono .paket/paket.exe update
-mono packages/FAKE/tools/FAKE.exe build.fsx
+function build(){
+  mono .paket/paket.exe update
+  mono packages/FAKE/tools/FAKE.exe build.fsx
+}
+
+file=".paket/paket.exe"
+if [ -f "$file" ]
+then
+{
+  build
+}
+else
+  mono .paket/paket.bootstrapper.exe
+  build
+fi
